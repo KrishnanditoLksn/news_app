4b00505888e541d0de028b3d1134b83e9ba9b0c4
KrishnanditoLksn <krislaksana123@gmail.com>
2025-07-28 20:09:04 +0700

    feat:implement list of news using ListView

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:starwars/model/news_model.dart';
+import 'package:starwars/services/api_service.dart';
 
 class MyHome extends StatefulWidget {
   const MyHome({super.key});
@@ -8,6 +10,7 @@ class MyHome extends StatefulWidget {
 }
 
 class MyHomeState extends State<MyHome> {
+  ApiService client = ApiService();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +19,25 @@ class MyHomeState extends State<MyHome> {
           "News App Lesgo",
           style: TextStyle(color: const Color.fromARGB(255, 26, 84, 131)),
         ),
+      ),
+
+      body: FutureBuilder(
+        future: client.getNews(),
+        builder: (BuildContext context, AsyncSnapshot<List<News>> snapshot) {
+          if (snapshot.hasData) {
+            List<News>? articles = snapshot.data;
+            return ListView.builder(
+              itemCount: articles!.length,
+              itemBuilder:
+                  (context, index) =>
+                      ListTile(title: Text(articles[index].title!)),
+            );
+          }
+          if (snapshot.hasError) {
+            return Text("Error ${snapshot.error}");
+          }
+          return Center(child: CircularProgressIndicator());
+        },
       ),
     );
   }
