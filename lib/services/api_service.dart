740dd4d5d9714871d76cbb952ed1450a1b043d60
KrishnanditoLksn <krislaksana123@gmail.com>
2025-07-28 20:08:38 +0700

    feat:fetch news data from api

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,7 +1,32 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:starwars/api/api_key.dart';
+
+import 'package:starwars/model/news_model.dart';
 
 class ApiService {
   final baseUrl = "newsapi.org";
   final client = http.Client();
-  
+
+  Future<List<News>> getNews() async {
+    final queryParams = {
+      'country': 'us',
+      'category': 'technology',
+      'apikey': apikey,
+    };
+
+    final uri = Uri.https(baseUrl, '/v2/top-headlines', queryParams);
+    final response = await client.get(uri);
+
+    /*
+    Decode json body
+    */
+
+    Map<String, dynamic> json = jsonDecode(response.body);
+    List<dynamic> body = json['articles'];
+
+    List<News> news = body.map((dynamic item) => News.fromJson(item)).toList();
+    return news;
+  }
 }
