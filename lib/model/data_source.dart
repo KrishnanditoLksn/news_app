f879a0cf168de9e27d23f102c004d0f76f87ccf1
KrishnanditoLksn <krislaksana123@gmail.com>
2025-07-30 21:59:07 +0700

    refactor:separate into screen  and data pacckage

--- lib/model/data_source.dart
+++
@@ -1,9 +0,0 @@
-class DataSource {
-  String? id;
-  String? name;
-
-  DataSource({this.id, this.name});
-  factory DataSource.fromJson(Map<String, dynamic> json) {
-    return DataSource(id: json['id'], name: json['name']);
-  }
-}
