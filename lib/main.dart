de7fe6737772ca57e073ebdf8c280c4696d2b432
KrishnanditoLksn <krislaksana123@gmail.com>
2025-08-04 22:49:42 +0700

    feat:gemini init

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:starwars/screens/main/main_page.dart';
-
+import 'package:starwars/services/api/api_key.dart';
 
 void main() {
+  Gemini.init(
+    apiKey: geminiApiKey);
   runApp(const MyApp());
 }
 
@@ -10,9 +13,6 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MainPage(),
-      debugShowCheckedModeBanner: false
-      );
+    return MaterialApp(home: MainPage(), debugShowCheckedModeBanner: false);
   }
 }
