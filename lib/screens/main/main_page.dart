8a4f6a4b191df661e4dc5dd426ec825ad2e53bd1
KrishnanditoLksn <krislaksana123@gmail.com>
2025-08-06 13:58:45 +0700

    refactor:used bottom navy bar (custom) instead pure from its framework

--- lib/screens/main/main_page.dart
+++ lib/screens/main/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:starwars/screens/home/home.dart';
 import 'package:starwars/screens/notification/news_notification.dart';
@@ -12,38 +13,65 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   int _selectedIndex = 0;
 
-  List<Widget> widgetList = <Widget>[MyHome(),NewsNotification()];
+  late PageController _pageController;
+
+  List<Widget> widgetList = <Widget>[MyHome(), NewsNotification()];
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        showSelectedLabels: false,
-        showUnselectedLabels: true,
+      body: SizedBox.expand(
+        child: PageView(
+          controller: _pageController,
+          onPageChanged:
+              (value) => {
+                setState(() {
+                  _selectedIndex = value;
+                }),
+              },
+          children: [MyHome(), NewsNotification()],
+        ),
+      ),
+
+      bottomNavigationBar: BottomNavyBar(
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        showElevation: true,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        selectedIndex: _selectedIndex,
+        onItemSelected:
+            (index) => setState(() {
+              _selectedIndex = index;
+              _pageController.animateToPage(
+                index,
+                duration: Duration(milliseconds: 300),
+                curve: Curves.ease,
+              );
+            }),
         items: [
-          BottomNavigationBarItem(
-            label: "Health",
-            icon: Icon(Icons.health_and_safety, color: Colors.green),
-          ),
-          BottomNavigationBarItem(
-            label: "Everything News",
+          BottomNavyBarItem(
+            activeColor: Colors.green,
+            title: Text("News", style: TextStyle(fontFamily: "Poppins")),
             icon: Icon(
-              Icons.topic,
-              color: const Color.fromARGB(255, 91, 165, 144),
+              Icons.newspaper,
+              color: const Color.fromARGB(255, 71, 16, 2),
+            ),
+          ),
+          BottomNavyBarItem(
+            title: Text("Settings", style: TextStyle(fontFamily: "Poppins")),
+            icon: Icon(
+              Icons.settings,
+              color: const Color.fromARGB(255, 1, 25, 18),
             ),
           ),
         ],
-
-        //set state change when index is active while clicked
-        onTap: (idx) {
-          setState(() {
-            _selectedIndex = idx;
-          });
-        },
       ),
-      body: widgetList.elementAt(_selectedIndex),
     );
   }
 }
