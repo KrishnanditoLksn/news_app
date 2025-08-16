7ffdbeb7373b589e6aaefdd1806e3df53b93d6c5
KrishnanditoLksn <krislaksana123@gmail.com>
2025-08-16 11:28:38 +0700

    feat:call change notifier provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
+import 'package:provider/provider.dart';
+import 'package:starwars/provider/gemini_chat_provider.dart';
 import 'package:starwars/screens/main/main_page.dart';
 import 'package:starwars/services/api/api_key.dart';
+import 'package:starwars/services/api/gemini_api_service.dart';
 
 void main() {
-  Gemini.init(
-    apiKey: geminiApiKey);
-  runApp(const MyApp());
+  Gemini.init(apiKey: geminiApiKey);
+  runApp(
+    ChangeNotifierProvider<GeminiChatProvider>(
+      create: (context) => GeminiChatProvider(GeminiApiService()),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
