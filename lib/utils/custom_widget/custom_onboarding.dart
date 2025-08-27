b80f3e0af46da83317d59e7d6cbb6f46ff06f13e
KrishnanditoLksn <krislaksana123@gmail.com>
2025-08-27 12:43:01 +0700

    feat:add custom onboarding screen

---
+++ lib/utils/custom_widget/custom_onboarding.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class CustomOnboarding extends StatelessWidget {
+  final String imgSrc, onboardingTitle, descriptionOnboarding;
+
+  const CustomOnboarding(
+    this.imgSrc,
+    this.onboardingTitle,
+    this.descriptionOnboarding, {
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Container(
+            width: 442,
+            height: 200,
+            decoration: ShapeDecoration(
+              gradient: LinearGradient(
+                begin: Alignment(-0.52, -0.63),
+                end: Alignment(1.49, 1.65),
+                colors: [const Color(0xFF00FF83), Colors.white],
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(100),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 30),
+          Image.asset(width: 250, height: 250, imgSrc),
+          SizedBox(height: 100),
+          Column(
+            children: [
+              Text("Onboarding 1 "),
+              Text(onboardingTitle),
+              SizedBox(height: 150),
+              // ElevatedButton(
+              //   onPressed: () => {},
+              //   child: Text(
+              //     "Summarize this text",
+              //     style: TextStyle(fontFamily: "Poppins", fontSize: 20),
+              //   ),
+              // ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
